13b1f144242a3adb2adb8d522f31697bbda8a43e
Raof86 <67486433+Raof86@users.noreply.github.com>
2021-08-27 15:45:11 +0200

    update

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 let element_1 = document.getElementById('qwerty');
 let element_2 = document.getElementById('phrase');
 let element_3 = document.querySelector('.btn__reset');
-const overlay =document.querySelector('#overlay');
+const overlay = document.querySelector('#overlay');
 
-var missed_variable = 0;
+let missed_variable = 0;
 
-var phrase = ['JavaScript', 'Ruby', 'Golang', 'Java', 'JQuery'];
+const phrase = ['JavaScript', 'Ruby', 'Golang', 'Java', 'JQuery'];
 
+
+// return a random phrase from an array
+const getRandomPhraseAsArray = arr => {
+   let randomNumber = Math.floor(Math.random() * phrase.length)
+    arr = phrase[randomNumber];
+}
+
+// adds the letters of a string to the display
+const addPhraseToDisplay = arr => {
+}
+
+// check if a letter is in the phrase
+const checkLetter = button => {
+}
+
+// check if the game has been won or lost 
+const checkWin = () => {
+}
+
+// listen for the start game button to be pressed
 element_3.addEventListener('click', () => 
+overlay.style.display="none"
+)
 
-   {
-      function myFunction() {
-         if (true) 
-         document.querySelector("#overlay").style.display="none";
-       
-         else (false)
-         document.querySelector("#overlay").style.display="flex";
-      }
-   }
-      
- )
+// listen for the onscreen keybord to be clicked 
+qwerty.addEventListener('click', e => {});
 
